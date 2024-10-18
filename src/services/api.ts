5f088afac04925a5706b20cd52a906ba1857fabe
hsieh-sunghai <tsia.sionghai@gmail.com>
2024-10-19 07:21:27 +0800

    Create api.ts

---
+++ src/services/api.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'https://api.ezread.com.tw';
+
+const api = axios.create({
+  baseURL: API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export const uploadImage = async (file: File) => {
+  const formData = new FormData();
+  formData.append('image', file);
+  const response = await api.post('/image/toE6bin', formData);
+  return response.data;
+};
+
+export const generateConfig = async (config: any) => {
+  const response = await api.post('/generate-config', config);
+  return response.data;
+};
+
+// 添加其他 API 調用函數...
+
+export default api;
