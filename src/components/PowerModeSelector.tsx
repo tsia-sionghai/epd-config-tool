82f3e1af740ff7c17a17b023d89b0eedb02a75e0
hsieh-sunghai <tsia.sionghai@gmail.com>
2024-10-22 15:18:33 +0800

    Upgrade PowerModeSelector component

--- src/components/PowerModeSelector.tsx
+++ src/components/PowerModeSelector.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import SelectableButton from './SelectableButton';
-import PowerOffIcon from '@mui/icons-material/PowerOff';
-import BedtimeIcon from '@mui/icons-material/Bedtime';
-import AutoFixNormalIcon from '@mui/icons-material/AutoFixNormal';
+
+// 導入自定義圖片
+import offPwerModeIcon from '../assets/img_poweroff.png';
+import sleepPowerModeIcon from '../assets/img_sleep.png';
+import normalPowerModeIcon from '../assets/img_wake.png';
 
 interface ModeSelectorProps {
   value: 'normal' | 'hibernation' | 'off';
@@ -11,36 +13,38 @@ interface ModeSelectorProps {
 }
 
 const PowerModeSelector: React.FC<ModeSelectorProps> = ({ value, onChange }) => {
+  const modeOptions = [
+    {
+      mode: 'off',
+      icon: offPwerModeIcon,
+      title: 'Off Mode',
+      description: '畫面刷新後，機台將會關機。'
+    },
+    {
+      mode: 'hibernation',
+      icon: sleepPowerModeIcon,
+      title: 'Sleep Mode',
+      description: '畫面刷新後，機台將會進入睡眠模式。'
+    },
+    {
+      mode: 'normal',
+      icon: normalPowerModeIcon,
+      title: 'Normal Mode',
+      description: '畫面刷新後，機台將會持續醒著。'
+    }
+  ];
+
   return (
     <Grid container spacing={2}>
-      {[
-        {
-          mode: 'off',
-          icon: <PowerOffIcon />,
-          title: 'Off Mode',
-          description: '畫面刷新後，機台將會關機。'
-        },
-        {
-          mode: 'hibernation',
-          icon: <BedtimeIcon />,
-          title: 'Sleep Mode',
-          description: '畫面刷新後，機台將會進入睡眠模式。'
-        },
-        {
-          mode: 'normal',
-          icon: <AutoFixNormalIcon />,
-          title: 'Normal Mode',
-          description: '畫面刷新後，機台將會持續醒著。'
-        },
-      ].map((item) => (
+      {modeOptions.map((item) => (
         <Grid item xs={4} key={item.mode}>
           <SelectableButton
             selected={value === item.mode}
-            onClick={() => onChange(item.mode as 'normal' | 'hibernation' | 'off')}
-            icon={item.icon}
+            onClick={() => onChange(item.mode)}
+            icon={<img src={item.icon} alt={item.title} style={{ width: 48, height: 48 }} />}
             title={item.title}
             description={item.description}
-            sx={{ height: '100%' }}  // 確保每個按鈕填滿 Grid item 的高度
+            sx={{ height: '100%' }}
           />
         </Grid>
       ))}
